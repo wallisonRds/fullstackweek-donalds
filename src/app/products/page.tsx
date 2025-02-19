f6cacda60155ee1eb16c0d18458886a704337310
WALLISON VIEIRA SANTOS PEREIRA <wallisonn13@gmail.com>
2025-02-19 11:14:58 -0300

    feat: add shadcn initial setup and components

---
+++ src/app/products/page.tsx
@@ -0,0 +1,14 @@
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+
+const ProductPage = () => {
+  return (
+    <div className="p-5 border border-red-500 rounded-xl">
+      <h1 className="text-red-500">products page</h1>
+      <Button>FSW 7.0</Button>
+      <Input placeholder="Bora fechar esse projeto!" />
+    </div>
+  );
+};
+
+export default ProductPage;
