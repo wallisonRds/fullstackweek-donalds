2abb49ba83a0ad507a4146520fe94bc33eac8d89
WALLISON VIEIRA SANTOS PEREIRA <wallisonn13@gmail.com>
2025-02-22 00:16:35 -0300

    feat: ensure same product can be added more than once to cart

--- src/app/[slug]/menu/contexts/card.tsx
+++ src/app/[slug]/menu/contexts/card.tsx
@@ -32,8 +32,22 @@ export const CartProvider = ({ children }: { children: ReactNode }) => {
     };
 
     const addProduct = (product: CartProduct) => {
-        setProducts(prev => ([...prev, product]));
-    }
+        const productIsAlreadyOnTheCart = products.some((prevProduct) => prevProduct.id == product.id,);
+        if (!productIsAlreadyOnTheCart) {
+            return setProducts((prev) => [...prev, product]);
+        }
+        setProducts(prevProducts => {
+            return prevProducts.map(prevProduct => {
+                if (prevProduct.id == product.id) {
+                return {
+                   ...prevProduct,
+                   quantity: prevProduct.quantity + product.quantity
+        }
+     }
+     return prevProduct
+    })
+  })
+};
 return (
     <CartContext.Provider value={{
         isOpen,
