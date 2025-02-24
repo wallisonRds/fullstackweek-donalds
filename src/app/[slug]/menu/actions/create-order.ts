baeab103f44776fe9dfc50e8df21573c4a6ae46b
WALLISON VIEIRA SANTOS PEREIRA <wallisonn13@gmail.com>
2025-02-23 21:53:26 -0300

    feat: add orders page

--- src/app/[slug]/menu/actions/create-order.ts
+++ src/app/[slug]/menu/actions/create-order.ts
@@ -1,6 +1,7 @@
 "use server";
 
 import { ConsumptionMethod } from "@prisma/client";
+import { redirect } from "next/navigation";
 
 import { db } from "@/lib/prisma";
 
@@ -58,4 +59,5 @@ export const createOrder = async (input: CreateOrderInput) => {
       restaurantId: restaurant.id,
     },
   });
+  redirect(`/${input.slug}/orders`);
 };
