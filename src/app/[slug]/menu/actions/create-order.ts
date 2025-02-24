65e9b475f2befa2b40cf2e755967a6a52d44cdf7
WALLISON VIEIRA SANTOS PEREIRA <wallisonn13@gmail.com>
2025-02-23 22:29:10 -0300

    feat: ensure cache is revalidated when creating an order

--- src/app/[slug]/menu/actions/create-order.ts
+++ src/app/[slug]/menu/actions/create-order.ts
@@ -1,6 +1,7 @@
 "use server";
 
 import { ConsumptionMethod } from "@prisma/client";
+import { revalidatePath } from "next/cache";
 import { redirect } from "next/navigation";
 
 import { db } from "@/lib/prisma";
@@ -59,5 +60,6 @@ export const createOrder = async (input: CreateOrderInput) => {
       restaurantId: restaurant.id,
     },
   });
+  revalidatePath(`/${input.slug}/orders`);
   redirect(`/${input.slug}/orders?cpf=${removeCpfPunctuation(input.customerCpf)}`);
 };
