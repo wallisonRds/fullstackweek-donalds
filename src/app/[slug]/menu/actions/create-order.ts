9d60fb4d250f8adf06beb57eb3dee3fb68609358
WALLISON VIEIRA SANTOS PEREIRA <wallisonn13@gmail.com>
2025-02-23 21:58:15 -0300

    fix: ensure user is redirected with cpf after order creation

--- src/app/[slug]/menu/actions/create-order.ts
+++ src/app/[slug]/menu/actions/create-order.ts
@@ -59,5 +59,5 @@ export const createOrder = async (input: CreateOrderInput) => {
       restaurantId: restaurant.id,
     },
   });
-  redirect(`/${input.slug}/orders`);
+  redirect(`/${input.slug}/orders?cpf=${removeCpfPunctuation(input.customerCpf)}`);
 };
--- src/app/[slug]/orders/page.tsx
+++ src/app/[slug]/orders/page.tsx
@@ -17,6 +17,9 @@ const OrdersPage = async ({searchParams}: OrdersPageProps) => {
      return  <CpfForm />;
     }
     const orders = await db.order.findMany({
+        orderBy: {
+            createdAt: "desc",
+        },
         where: {
             customerCpf: removeCpfPunctuation(cpf)
         },
